c4c87796736665b9859becd2c877cce48748f5a5
darja <darja.portnova@gmail.com>
2024-11-23 16:14:17 +0200

    initial commit

--- BackendApi/index.js
+++ BackendApi/index.js
@@ -1,7 +1,6 @@
 const app = require('express')();
 const port = 8080;
-const swaggerUI = require
-(`swagger-ui-express`);
+const swaggerUI = require(`swagger-ui-express`);
 const swaggerDocument = require('./docs/swagger.json');
 
 app.use('/docs', swaggerUi.serve, swaggerUI.setup(swaggerDocument))
