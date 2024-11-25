07ff2187ed480de4f49824745e9621cf03dcb89e
darja <darja.portnova@gmail.com>
2024-11-25 10:19:21 +0200

    https://trello.com/c/PBXyBevW/9-as-an-developer-i-want-to-see-the-full-list-of-movies-in-my-app

--- BackendApi/index.js
+++ BackendApi/index.js
@@ -3,6 +3,10 @@ const port = 8080;
 const swaggerUI = require(`swagger-ui-express`);
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('movies', (req, res) => {
+    res.send(["Harry Potter", "dfs", "sgd", "sgd",])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUI.setup(swaggerDocument))
 
 
