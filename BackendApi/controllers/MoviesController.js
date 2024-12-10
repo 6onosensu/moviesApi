a66897e3e6928bbbb7413d2f9b5f9f728a2dc05b
darja <darja.portnova@gmail.com>
2024-12-10 14:24:26 +0200

    a small refactoring

--- BackendApi/controllers/MoviesController.js
+++ BackendApi/controllers/MoviesController.js
@@ -1,6 +1,81 @@
 const {db} = require('../db');
 const Utils = require('./utils');
 
+const movies = [
+    {
+        movieID: 1, 
+        name: "Harry Potter and the Philosopher's Stone",
+        description: "A young boy discovers he is a wizard. He begins his magical journey at Hogwarts School of Witchcraft and Wizardry.",
+        year: 2001,
+        genres: [1, 2, 3],
+        directors: [3],
+        actors: [1, 2, 3],
+    }, 
+    {
+        movieID: 2, 
+        name: "Harry Potter and the Chamber of Secrets",
+        description: "Harry returns to Hogwarts for his second year. He uncovers the truth behind a dark chamber in the school.",
+        year: 2002,
+        genres: [1, 2, 4],
+        directors: [],
+        actors: [],
+    }, 
+    {
+        movieID: 3, 
+        name: "Harry Potter and the Prisoner of Azkaban",
+        description: "Harry learns about his connection to a dangerous prisoner. He discovers more about his past and family secrets.",
+        year: 2004,
+        genres: [1, 2, 5],
+        directors: [],
+        actors: [],
+    }, 
+    {
+        movieID: 4, 
+        name: "Harry Potter and the Goblet of Fire",
+        description: "Harry is mysteriously entered into a dangerous tournament. He faces deadly challenges and the return of Voldemort.",
+        year: 2005,
+        genres: [1, 2, 6],
+        directors: [],
+        actors: [],
+    },
+    {
+        movieID: 5, 
+        name: "Harry Potter and the Order of the Phoenix",
+        description: "Harry forms a secret group to fight against Voldemort. He confronts the Ministry and uncovers the prophecy about him.",
+        year: 2007,
+        genres: [1, 2, 7],
+        directors: [],
+        actors: [],
+    },
+    {
+        movieID: 6, 
+        name: "Harry Potter and the Half-Blood Prince",
+        description: "Harry delves into Voldemort's past through memories. He discovers secrets about Horcruxes that can defeat him.",
+        year: 2009,
+        genres: [1, 2, 8],
+        directors: [],
+        actors: [],
+    },
+    {
+        movieID: 7, 
+        name: "Harry Potter and the Deathly Hallows: Part 1",
+        description: "Harry, Ron, and Hermione search for Horcruxes. They face betrayal, danger, and growing threats from Voldemort.",
+        year: 2010,
+        genres: [1, 2, 6],
+        directors: [],
+        actors: [],
+    },
+    {
+        movieID: 8, 
+        name: "Harry Potter and the Deathly Hallows: Part 2",
+        description: "The epic final battle between Harry and Voldemort unfolds. Hogwarts becomes the stage for the ultimate showdown.",
+        year: 2011,
+        genres: [1, 2, 7],
+        directors: [],
+        actors: [],
+    }
+]
+
 exports.getAll = 
 async (req, res) => {
     res.status(200).send(movies.map(({movieID, name}) => ({ movieID, name })));
